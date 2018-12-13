70b54afac33dca30d17f6b06bc8342af1f84f8ee
Handrews0101 <32401789+Handrews0101@users.noreply.github.com>
2018-12-12 23:58:04 -0500

    Update Main.java

--- databaseProject-master/BooksFX/src/sample/Main.java
+++ databaseProject-master/BooksFX/src/sample/Main.java
@@ -7,7 +7,8 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+// Override tells the main part of where is is overriding, that it is overriding 
+    // then when it compiles, the compiler doesn't freak out 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
@@ -15,8 +16,6 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
-
-
     public static void main(String[] args) {
         launch(args);
     }
